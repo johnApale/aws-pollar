ccc2dc2e55c4107c5920c52952435c2c6536e57a
MTLiew <maxl24usa@gmail.com>
2022-03-01 00:57:55 -0800

    Custom Log In Components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,218 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { withAuthenticator } from "@aws-amplify/ui-react";
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./pages/Home/Home";
 import Test from "./pages/Test/Test";
 import Search from "./pages/Search/Search";
 import CreatePoll from "./pages/Poll/CreatePoll";
 
-function App({ user, signOut }) {
+import { Authenticator, useTheme, useAuthenticator} from "@aws-amplify/ui-react";
+import { Amplify } from 'aws-amplify';
+
+import awsExports from './aws-exports';
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure(awsExports);
+
+const components = {
+  Header() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Image
+          alt="Amplify logo"
+          src="https://docs.amplify.aws/assets/logo-dark.svg"
+        />
+      </View>
+    );
+  },
+
+  Footer() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Text color={`${tokens.colors.neutral['80']}`}>
+          &copy; All Rights Reserved
+        </Text>
+      </View>
+    );
+  },
+
+  SignIn: {
+    Header() {
+      const { tokens } = useTheme();
+
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Sign in to your account
+        </Heading>
+      );
+    },
+    
+    Footer() {
+      const { toResetPassword } = useAuthenticator();
+
+      return (
+        <View textAlign="center">
+          <Button
+            fontWeight="normal"
+            onClick={toResetPassword}
+            size="small"
+            variation="link"
+          >
+            Reset Password
+          </Button>
+        </View>
+      );
+    },
+  },
+
+  SignUp: {
+    Header() {
+      const { tokens } = useTheme();
+
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Create a new account
+        </Heading>
+      );
+    },
+    Footer() {
+      const { toSignIn } = useAuthenticator();
+
+      return (
+        <View textAlign="center">
+          <Button
+            fontWeight="normal"
+            onClick={toSignIn}
+            size="small"
+            variation="link"
+          >
+            Back to Sign In
+          </Button>
+        </View>
+      );
+    },
+  },
+  
+  ConfirmSignUp: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+  
+  SetupTOTP: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+  
+  ConfirmSignIn: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+  
+  ResetPassword: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+  
+  ConfirmResetPassword: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    
+    Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+};
+
+export default function App() {
   return (
-    <div className="App">
-      <Router>
-        <nav>
-          <NavBar signOut={signOut} />
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home user={user} />}></Route>
-          <Route path="poll/create" element={<CreatePoll user={user}/>}></Route>
-          <Route path="test" element={<Test user={user} />}></Route>
-          <Route
-            path="search_results/:query"
-            element={<Search user={user} />}
-          ></Route>
-        </Routes>
-      </Router>
-    </div>
+    <Authenticator components={components}>
+      {({ signOut, user }) => (
+        <div className="App">
+          <Router>
+            <nav>
+              <NavBar signOut={signOut} />
+            </nav>
+            
+            <Routes>
+              <Route path="/" element={<Home user={user} />}></Route>
+              <Route path="poll/create" element={<CreatePoll user={user}/>}></Route>
+              <Route path="test" element={<Test user={user} />}></Route>
+              <Route
+                path="search_results/:query"
+                element={<Search user={user} />}
+              ></Route>
+            </Routes>
+          </Router>
+        </div>
+        )
+      }
+    </Authenticator>
   );
 }
-
-export default withAuthenticator(App);
