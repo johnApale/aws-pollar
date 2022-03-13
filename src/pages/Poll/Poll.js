f84dff04f8b801da731efaa32700c735b19fe5d1
ColinRayburn <47834560+ColinRayburn@users.noreply.github.com>
2022-03-13 15:31:04 -0700

    figured some anser stuff out

--- src/pages/Poll/Poll.js
+++ src/pages/Poll/Poll.js
@@ -2,35 +2,29 @@ import React, { useState, useEffect } from "react";
 import "./Poll.css"
 import { DataStore } from "aws-amplify";
 import { Poll, Sex, UserInformation } from "../../models";
+import { useNavigate } from "react-router-dom"
 
 function AnswerPoll(props) {
-  const [creatorInfo, setCreatorInfo] = useState([]);
-  const [creator, setCreator] = useState([]);
+  const nav = useNavigate();
+  const [numAnswers, setNumAnswer] = useState();
   const [postData, setPostData] = useState([]);
   const [post, setPost] = useState([]);
-  const [answers, setAnswers] = useState([]);
+  //const timer = setTimeout(() => console.log('Initial timeout!'), 10000);
+  
+  
   
 
 
   useEffect(async () => {
-    setPostData(
-      await DataStore.query(Poll, (p) => 
-        p.id("eq", "942c8a6e-1cd1-4077-aa5b-6eb0d4f3dd77")
-      )
-    );
-    postData.map((val, key) => {
-      setPost({
-        id: val.id,
-        userInformationID: val.userInformationID,
-        title: val.title,
-        pollUserInformation: val.UserInformation,
-        description: val.description,
-        answerList: val.answerChoices,
-        likes: val.likes,
-        views: val.views,
-
-      });
-    });
+    try{   
+      const model = await DataStore.query(Poll, (p) => p.id("eq", "942c8a6e-1cd1-4077-aa5b-6eb0d4f3dd77"));
+      setPostData(model);
+    }
+    catch (error){
+      console.log(error);
+    }
+    },[]);
+    
 
     
     //setAnswers(post.answerChoices);
@@ -48,29 +42,7 @@ function AnswerPoll(props) {
     //   });
     // });
 
-  });
-
-  const getCreator = async () =>{
-    setCreatorInfo(
-      await DataStore.query(UserInformation, (u) => u.id("eq", post.userInformationID))
-    );
-    creatorInfo.map((val, key) => {
-      setCreator({
-        id: val.id,
-        username: val.username,
-      });
-    });
-
-    return creator.username;
-  }
-
-  // const queryData = async () => {
-  //   const models = await DataStore.query(Poll, (p) =>
-  //     p.id("eq", "942c8a6e-1cd1-4077-aa5b-6eb0d4f3dd77")
-  //   );
-    
-  //   setPost(models);
-  // };
+  
 
 
 
@@ -79,59 +51,63 @@ function AnswerPoll(props) {
       <body>
       <div class = "form">
         <form class = "answerPoll">
+          {postData.map((val, key) => {
+            return(
+              <><span class="username">
+                <p1> Creator: {val.UserInformation.username}</p1>
+              </span><br /><h1>Title:{val.title}</h1><div class="description">
+                </div><div class="pollOptions">
+                  <label class="options">
+                    <input type="radio" name="radio" id="option1"></input>
+                    <span class="selectOption">{val.answerChoices[0]}</span>
+                  </label><br />
+                  <label class="options">
+                    <input type="radio" name="radio" id="option2" />
+                    <span class="selectOption">{val.answerChoices[1]}</span>
+                  </label><br />
+                  {val.answerChoices[2] !== "" &&(
+                    <><label class="options">
+                      <input type="radio" name="radio" id="option3" />
+                      <span class="selectOption">{val.answerChoices[2]}</span>
+                    </label><br /></>
+                  )}
+                  {val.answerChoices[3] !== "" &&(
+                    <><label class="options">
+                        <input type="radio" name="radio" id="option4" />
+                        <span class="selectOption">{val.answerChoices[3]}</span>
+                      </label><br /></>
+                  )}
+                  {val.answerChoices[4] !== "" &&(
+                    <><label class="options">
+                        <input type="radio" name="radio" id="option5" />
+                        <span class="selectOption">{val.answerChoices[4]}</span>
+                      </label><br /></>
+                  )}
+                  {val.answerChoices[5] !== "" &&(
+                    <><label class="options">
+                        <input type="radio" name="radio" id="option6" />
+                        <span class="selectOption">{val.answerChoices[5]}</span>
+                      </label><br /></>
+                  )}
+                  {val.answerChoices[6] !== "" &&(
+                    <><label class="options">
+                        <input type="radio" name="radio" id="option7" />
+                        <span class="selectOption">{val.answerChoices[6]}</span>
+                      </label><br /></>
+                  )}
+                  {val.answerChoices[7] !== "" &&(
+                    <><label class="options">
+                        <input type="radio" name="radio" id="option8" />
+                        <span class="selectOption">{val.answerChoices[7]}</span>
+                      </label><br /></>
+                  )}
 
-
-      
-        <span class = "username">
-          <p1> Creator: {post.userInformationID}</p1>
-        </span>
-
-      
-        <br/><h1>Title:{post.title}</h1>
-
-      
-        <div class = "description">
-          <p2> Description: {post.description}</p2><br></br>
-        </div>
-
-      
-        <div class = "pollOptions">
-          <label class = "options"> 
-            <input type = "radio" name = "radio" id = "option1"></input>
-            <span class = "selectOption">option1:{post.answerList[0]}</span>
-          </label><br/>
-          <label class = "options"> 
-            <input type = "radio" name = "radio" id = "option2"/>
-            <span class = "selectOption">option2:{post.answerList[1]}</span>
-          </label><br/>
-          <label class = "options"> 
-            <input type = "radio" name = "radio" id = "option3"/>
-            <span class = "selectOption">option3</span>
-          </label><br/>
-          <label class = "options"> 
-            <input type = "radio" name = "radio" id = "option4"/>
-            <span class = "selectOption">option4</span>
-          </label><br/>
-          <label class = "options"> 
-            <input type = "radio" name = "radio" id = "option5"/>
-            <span class = "selectOption">option5</span>
-          </label><br/>
-          <label class = "options"> 
-            <input type = "radio" name = "radio" id = "option6"/>
-            <span class = "selectOption">option6</span>
-          </label><br/>
-          <label class = "options"> 
-            <input type = "radio" name = "radio" id = "option7"/>
-            <span class = "selectOption">option7</span>
-          </label><br/>
-          <label class = "options"> 
-            <input type = "radio" name = "radio" id = "option8"/>
-            <span class = "selectOption">option8</span>
-          </label><br/>
-        </div>
-      <div class = "pollVote">
-        <br/><input type = "submit" id = "submitAns" value = "Vote"/>
-      </div>
+                </div><div class="pollVote">
+                  <br /><input type="submit" id="submitAns" value="Vote" onClick={(event) => {nav("/");}}/>
+                </div></>
+            );
+          })}
+        
     </form>
   </div>
   <br/><div class = "userOptions" >
