88e8c07a14b244596343929fb048760927f98a4c
caitlinmart <caitlinmart@users.noreply.github.com>
2022-03-25 18:09:32 -0700

    user followers and following

---
+++ src/pages/User/UserSubscriptions.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Following.css"
+
+
+
+function UserSubscriptions() {
+  return (
+    <div className="user_subscriptions">
+        <div className = "useraccount">
+            <img src="/profilepic1.png" className="user_avatar" />
+            
+            <a className = "user_link"> username</a>
+        </div>
+    
+    </div>
+  );
+}
+
+export default UserSubscriptions;
